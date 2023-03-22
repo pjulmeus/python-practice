52a9d2a58dd9c84820961796b200bdd53a9c63b8
Paul Julmeus <pjulmeus@Pauls-MBP.attlocal.net>
2023-03-22 14:12:50 -0400

    come back

--- 34_same_frequency/same_frequency.py
+++ 34_same_frequency/same_frequency.py
@@ -12,8 +12,19 @@ def same_frequency(num1, num2):
     """
     #  make the list a set 
     dict1 = {}
-    print(num1.split(""))
-    for n1 in num1:
-        print(n1)
-        dict[n1] = num1.count(n1)
+    dict2 = {}
+    str1 = (str(num1))
+    str2 = (str(num2))
+    set1 = (set(str1))
+    set2 = set(str2)
+    
+
+    if len(str1) is not len(str2): 
+        return False
+    else:
+        for n1 in set1:
+            if dict1[n1] in dict1:
+                dict1[n1] += 1
+            else:
+                dict1[n1] = 1
     return dict
